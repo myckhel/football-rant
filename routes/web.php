0a6c014c01f34974148c7557ece81b7122822a60
Hoffenheim Technologies <myckhel1@hotmail.com>
2018-10-04 18:37:47 +0100

    change root to teams

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ClubController@teams');
 
 Route::get('/login-register', function () {
     return view('auth.sign');
